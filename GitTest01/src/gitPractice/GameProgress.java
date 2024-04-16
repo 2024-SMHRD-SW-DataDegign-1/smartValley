afd654b8c4d7d7cccaaf367b95b0ed5610fc0e2a
smhrd <smhrd@DESKTOP-KJLQ3LV>
2024-04-16 17:25:21 +0900

    형동 - 신규 클래스 'GameProgress' 생성

---
+++ GitTest01/src/gitPractice/GameProgress.java
@@ -0,0 +1,15 @@
+package gitPractice;
+
+public class GameProgress {
+	
+	private int money;
+	private char[][] farm;
+	
+	public GameProgress(int money, char[][] farm) {
+		super();
+		this.money = money;
+		this.farm = farm;
+	}
+	
+	
+}
